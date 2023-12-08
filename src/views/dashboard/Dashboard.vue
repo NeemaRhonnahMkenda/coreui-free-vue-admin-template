422017fb7c5e50324de895b766c954dca9710b4e
Neema Rhonnah Mkenda <nmkenda98@gmail.com>
2023-12-08 17:35:15 +0300

    new changes

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -217,10 +217,12 @@ import avatar6 from '@/assets/images/avatars/6.jpg'
 import MainChart from './MainChart'
 import WidgetsStatsA from './../widgets/WidgetsStatsTypeA.vue'
 import WidgetsStatsD from './../widgets/WidgetsStatsTypeD.vue'
+import {CButtonGroup} from "@coreui/vue/dist/esm/components/button-group";
 
 export default {
   name: 'Dashboard',
   components: {
+    CButtonGroup,
     MainChart,
     WidgetsStatsA,
     WidgetsStatsD,
