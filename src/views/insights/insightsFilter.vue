3d50567c0681426d587636920065d7966e57e864
Neema Rhonnah Mkenda <115183776+NeemaRhonnahMkenda@users.noreply.github.com>
2023-12-08 11:43:57 +0000

    insights adjustments

--- src/views/insights/insightsFilter.vue
+++ src/views/insights/insightsFilter.vue
@@ -6,11 +6,30 @@
 </template>
 
 <script>
+import MainChart from './MainChart'
+import WidgetsStatsA from './../widgets/WidgetsStatsTypeA.vue'
+import WidgetsStatsD from './../widgets/WidgetsStatsTypeD.vue'
+
 export default {
   name: 'insightsFilter',
+  components: {
+    MainChart,
+    WidgetsStatsA,
+    WidgetsStatsD,
+  },
+  setup() {
+    const progressGroupExample1 = [
+      { title: 'Shots', value1: 34, value2: 78 },
+      { title: 'Interceptions', value1: 56, value2: 94 },
+      { title: 'Successful Tackles', value1: 12, value2: 67 },
+      { title: 'Progressive Passes', value1: 43, value2: 91 },
+      { title: 'Minutes PLayed', value1: 22, value2: 73 },
+      { title: 'Chances Created', value1: 53, value2: 82 },
+      { title: 'Touches', value1: 9, value2: 69 },
+    ]
+    return {
+      progressGroupExample1,
+    }
+  }
 }
 </script>
-
-<style scoped>
-/* Your component styles go here */
-</style>
